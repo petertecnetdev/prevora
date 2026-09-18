1b57f27963416e09452ad6513b7a1eab6c131206
Peter Tecnet <petertecnet@gmail.com>
2026-09-18 14:56:58 -0300

    chore(prevora): align eslint with JSX and resilient fallbacks

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,5 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   {ignores:['dist','node_modules']},
-  {files:['**/*.{js,jsx}'],languageOptions:{ecmaVersion:2022,globals:{...globals.browser,...globals.node},parserOptions:{ecmaFeatures:{jsx:true},sourceType:'module'}},plugins:{'react-hooks':reactHooks,'react-refresh':reactRefresh},rules:{...js.configs.recommended.rules,...reactHooks.configs.recommended.rules,'no-unused-vars':['warn',{argsIgnorePattern:'^_'}],'react-refresh/only-export-components':['warn',{allowConstantExport:true}]}}
+  {files:['**/*.{js,jsx}'],languageOptions:{ecmaVersion:2022,globals:{...globals.browser,...globals.node},parserOptions:{ecmaFeatures:{jsx:true},sourceType:'module'}},plugins:{'react-hooks':reactHooks,'react-refresh':reactRefresh},rules:{...js.configs.recommended.rules,...reactHooks.configs.recommended.rules,'no-unused-vars':'off','no-empty':['error',{allowEmptyCatch:true}],'react-hooks/exhaustive-deps':'warn','react-refresh/only-export-components':['warn',{allowConstantExport:true}]}}
 ]
